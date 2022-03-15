ae7fd272beb8a66925b88064fb840929267c20ea
Ignacio Oscar García <nachog.2000@hotmail.com>
2022-03-15 00:12:51 -0300

    Refactor

--- TP-Python-nivel-inicial/app_crud/borrar_empleado.py
+++ TP-Python-nivel-inicial/app_crud/borrar_empleado.py
@@ -5,15 +5,14 @@ def borrar(conexion, mi_id):
     cursor = conexion.cursor()
     mi_id = int(mi_id)
     data = (mi_id,)
-    sql_id = "SELECT COUNT(*) FROM empleados WHERE id = ?;"
+    sql_id = "SELECT * FROM empleados WHERE id = ?;"
     cursor.execute(sql_id, data)
-    rows = cursor.fetchall()
+    resultado_query = cursor.fetchone()
 
-    for row in rows:
-        if row[0] == 1:
-            sql = "DELETE from empleados where id = ?;"
-            cursor.execute(sql, data)
-            conexion.commit()
-            return True
-        else:
-            return False
+    if resultado_query is not None:
+        sql = "DELETE from empleados where id = ?;"
+        cursor.execute(sql, data)
+        conexion.commit()
+        return True
+    else:
+        return False
--- TP-Python-nivel-inicial/app_crud/insertar_empleado.py
+++ TP-Python-nivel-inicial/app_crud/insertar_empleado.py
@@ -3,19 +3,17 @@ import sqlite3
 
 def insertar(conexion, nombre, apellido, direccion, dni, telefono):
     cursor = conexion.cursor()
-    dni = int(dni)
     data = (dni,)
     sql_id = "SELECT COUNT(*) FROM empleados WHERE dni = ?;"
     cursor.execute(sql_id, data)
-    rows = cursor.fetchall()
+    resultado_query = cursor.fetchone()
 
-    for row in rows:
-        if row[0] == 0:
-            data = (nombre, apellido, direccion, dni, telefono)
-            sql = "INSERT INTO empleados(nombre, apellido, direccion, dni, telefono) \
+    if resultado_query is not None:
+        data_a_dar_alta = (nombre, apellido, direccion, dni, telefono)
+        sql = "INSERT INTO empleados(nombre, apellido, direccion, dni, telefono) \
             VALUES(?, ?, ?, ?, ?)"
-            cursor.execute(sql, data)
-            conexion.commit()
-            return True
-        else:
-            return False
+        cursor.execute(sql, data_a_dar_alta)
+        conexion.commit()
+        return True
+    else:
+        return False
