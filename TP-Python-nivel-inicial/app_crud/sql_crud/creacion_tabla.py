02b90eb87370b7f37a155604b6828aba510a2638
Ignacio Oscar García <nachog.2000@hotmail.com>
2022-03-16 23:48:12 -0300

    Falto agregar archivo

--- TP-Python-nivel-inicial/app_crud/sql_crud/creacion_tabla.py
+++ TP-Python-nivel-inicial/app_crud/sql_crud/creacion_tabla.py
@@ -1,10 +1,8 @@
-import sqlite3
-
-
 def crear_tabla(conexion):
 
     cursor = conexion.cursor()
-    sql = "CREATE TABLE IF NOT EXISTS empleados(id INTEGER PRIMARY KEY AUTOINCREMENT, nombre TEXT, \
-         apellido TEXT, direccion TEXT, dni INTEGER, telefono INTEGER)"
+    sql = "CREATE TABLE IF NOT EXISTS empleados(\
+         id INTEGER PRIMARY KEY AUTOINCREMENT, nombre TEXT, apellido TEXT,\
+               direccion TEXT, dni INTEGER, telefono INTEGER)"
     cursor.execute(sql)
     conexion.commit()
